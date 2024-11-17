6f1d4959b74f47a10ba0e7568219eed58d1afebf
Pantakan Rungwannarat <97592175+pantakan21@users.noreply.github.com>
2024-11-17 20:18:55 +0700

    Update

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,8 @@
 // export const development = "http://localhost:8080";
 // export const production = "vercel";
 
-export const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:8080" : "todo-list-backend-five-tawny.vercel.app";
+// export const PORT = process.env.PORT || 8080;
+
+export const apiUrl = process.env.NODE_ENV || "http://localhost:8080";
+
+// export const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:8080" : "todo-list-backend-five-tawny.vercel.app";
